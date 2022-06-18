6f007bdf04067ba37d17ac9bca89e38905a26ad2
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2022-06-18 17:02:35 +0300

    vuetify install

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,10 @@
 require('./bootstrap');
-
+import vuetify from './plugins/vuetify' // path to vuetify export
 window.Vue = require('vue').default;
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
     el: '#app',
+    vuetify,
 });
