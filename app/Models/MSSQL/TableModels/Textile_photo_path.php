61028faf5c227b79539abeaa9aedc232f4264640
Ilbko <ilbkopro60000@gmail.com>
2022-07-19 22:01:12 +0300

    table model add

--- app/Models/MSSQL/TableModels/Textile_photo_path.php
+++ app/Models/MSSQL/TableModels/Textile_photo_path.php
@@ -4,8 +4,8 @@ namespace App\Models\MSSQL\TableModels;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Photo_path extends Model
+class Textile_photo_path extends Model
 {
     protected $connection = 'mysql';
-    protected $table = 'kdfsnklw_woodTest.photo_path';
+    protected $table = 'kdfsnklw_woodTest.textile_photo_path';
 }
