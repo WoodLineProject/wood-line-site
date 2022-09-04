c401c972d23a802e9065472f96f230baa3b0a7f6
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2022-09-04 16:03:44 +0300

    icon from header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,10 @@
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Wood Line Factory</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/storage/app/public/image/icon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/storage/app/public/image/icon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/storage/app/public/image/icon/favicon-16x16.png">
+        <link type="image/png" sizes="192x192" rel="icon" href="/storage/app/public/image/icon/android-chrome-192x192.png">
     </head>
     <body class="antialiased">
         <div id="app"></div>
