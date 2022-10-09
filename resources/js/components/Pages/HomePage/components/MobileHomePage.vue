804a00e2591a67005e1e2bbe1dbc33583450e289
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2022-10-09 13:09:14 +0300

    fix title mobile home page

--- resources/js/components/Pages/HomePage/components/MobileHomePage.vue
+++ resources/js/components/Pages/HomePage/components/MobileHomePage.vue
@@ -59,7 +59,7 @@ export default {
                 <div class="d-flex flex-row">
                     <div id="lineInHomePage" class="my-auto"></div>
                     <v-card-title class="text-center">
-                        <span id="projectName" class="textInHomePage">{{ $t(`projectName`) }}</span>
+                        <span id="projectNameHomePage" class="textInHomePage">{{ $t(`projectName`) }}</span>
                     </v-card-title>
                     <div id="lineInHomePage" class="my-auto"></div>
                 </div>
